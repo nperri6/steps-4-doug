8eac908d235f5e809259e3dd8943439869bccf0f
Nick Perri <nperri6@gmail.com>
2018-01-14 13:45:24 -0600

    add scholarship blurb

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,11 @@ import styles from "./main.css";
 const IndexPage = () => (
 	<div className="wrapper">
 		<div className="banner">
-			<div className="bannerText">
-				THis is the banner
+			<div className="bannerTextContainer">
+				<div className="bannerText">
+				2018 Scholarship applications are now open!
+				</div>
+				<button className="greenButton"> Apply today </button>
 			</div>
 		</div>
 		<div className="mission"> </div>
